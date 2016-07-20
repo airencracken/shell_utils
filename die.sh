14b346768290083f195582a21b4b3e8fe14fbb4d
Marcus J. Hildum <marcus.hildum@gmail.com>
2016-07-20 15:54:24 -0700

    make die even more useful

--- die.sh
+++ die.sh
@@ -1,7 +1,11 @@
 #emulate perl's die
+#takes two arguments, a string and a int
 
-die(){
-	echo "$@" 1>&2
-	exit 1;
+die() {
+	default="shit is on fire yo"	
+	message="${1:-$default}"
+	status="${2:-1}"
+	echo $message 1>&2
+	exit ${status};
 }
 
