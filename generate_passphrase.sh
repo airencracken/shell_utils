56a6fa35f0fde0f5b95ffb969dbbb6462e2e68d9
Marcus J. Hildum <marcus.hildum@gmail.com>
2017-01-27 17:09:52 -0800

    strip trailing newline

--- generate_passphrase.sh
+++ generate_passphrase.sh
@@ -1,6 +1,6 @@
 #generate a pseudorandom passphrase of $1 words
 
 generate_passphrase() {
-	shuf -n ${1:-5} /usr/share/dict/words | tr '\n' ' '
+	shuf -n ${1:-5} /usr/share/dict/words | tr '\n' ' ' | head -c -1
 }
 
